39a56678405292777d02c05741357359b6b14624
Alessa-mo <871270392@qq.com>
2022-05-30 14:25:12 +0800

    新加用户名输入框

--- MyApplication/app/src/main/java/com/example/myapplication/LoginActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/LoginActivity.java
@@ -2,6 +2,7 @@ package com.example.myapplication;
 
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 //登录界面
@@ -17,12 +18,15 @@ public class LoginActivity extends AppCompatActivity {
         WINDOW_WIDTH = dm.widthPixels;
         WINDOW_HEIGHT = dm.heightPixels;
         PwdEdit pasEditText;
+        EditText userNameEdit;
 
         pasEditText = findViewById(R.id.ed_pas);
+        userNameEdit = findViewById(R.id.userName);
         pasEditText.setEditTextHint(getResources().getString(R.string.ed_hint_pas));
 
 //        获得输入内容
         String pas = pasEditText.getText();
+        String userName = userNameEdit.getText().toString();
         getSupportActionBar().hide();
     }
 }
